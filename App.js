cdf85d8ee4227f23c819a07e5ec4d75999838fc4
daniilnemtyrev <NemtyrevD@Mac-mini-admin.local>
2022-01-19 12:31:57 +0400

    first commit

--- App.js
+++ App.js
@@ -3,29 +3,47 @@ import {StyleSheet} from 'react-native';
 import {
   ViroARScene,
   ViroText,
+  ViroVideo,
   ViroConstants,
   ViroARSceneNavigator,
 } from '@viro-community/react-viro';
 
 const HelloWorldSceneAR = () => {
   const [text, setText] = useState('Initializing AR...');
+  const [x, setX] = useState(-3);
 
   function onInitialized(state, reason) {
     console.log('guncelleme', state, reason);
     if (state === ViroConstants.TRACKING_NORMAL) {
-      setText('Hello World!');
+      setText('ЯША КУСОК МЯСА ДЕРЬМАС ЭЛВИК ПИДАР');
     } else if (state === ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
     }
   }
 
+  const onClick = () => {
+    console.log(x);
+    if (x < 6) {
+      setX(x + 1);
+    } else {
+      setX(x - 1);
+    }
+  };
+
   return (
     <ViroARScene onTrackingUpdated={onInitialized}>
-      <ViroText
-        text={text}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
+      <ViroVideo
+        source={require('./video/2D.WebM.mp4')}
+        loop={true}
+        position={[x, -1, -2]}
+        onClick={onClick}
+        scale={[2, 3, 0]}
+        dragPlane={{
+          maxDistance: 10,
+          planePoint: [-3, -2, -1, 0, 1, 2, 3],
+          planeNormal: [-3, -2, -1, 0, 1, 2, 3],
+        }}
+        dragType={'FixedToWorld'}
       />
     </ViroARScene>
   );
